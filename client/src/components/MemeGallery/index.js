89175358fc2343ceb931ac087c4d43b172ffd9e1
Akaydia <akaydiaaa@hotmail.com>
2023-03-28 12:59:54 -0400

    got /memes to call 20 random gifs from giphy api

---
+++ client/src/components/MemeGallery/index.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+
+function GiphyGallery() {
+  const [gifs, setGifs] = useState([]);
+
+  useEffect(() => {
+    async function fetchGifs() {
+      const response = await fetch(
+        // 'https://api.giphy.com/v1/gifs/search?q=memes&api_key=SizPu7Wh56wn3UdCbKPje0GsFgd8wAGf&limit=20'
+        // 'https://api.giphy.com/v1/gifs/search?q=memes&api_key=YOUR_APIKEY&limit=20
+      );
+      const json = await response.json();
+      setGifs(json.data);
+    }
+    fetchGifs();
+  }, []);
+
+  return (
+    <div>
+      <h2>Popular Memes</h2>
+      <div className="gallery">
+        {gifs.map((gif) => (
+          <img
+            key={gif.id}
+            src={gif.images.downsized_medium.url}
+            alt={gif.title}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default GiphyGallery;
