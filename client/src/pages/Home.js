2cd92e7d81a9cf5969edfc1eca6979aba72bcb4c
Michael <fraynemichael300@gmail.com>
2023-03-29 09:21:20 -0600

    added 2 columns of gifs

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,18 +1,10 @@
-import { useQuery } from '@apollo/client';
-import Header from '../components/Header';
 import MemeGallery from '../components/MemeGallery';
-import { GET_MEMES } from '../utils/queries';
 
 function Home() {
-  const { loading, error, data } = useQuery(GET_MEMES);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error 🤦 </p>;
 
   return (
     <div>
-      <Header />
-      <MemeGallery memes={data.memes} />
+      <MemeGallery />
     </div>
   );
 }
