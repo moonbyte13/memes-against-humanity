9e975fe64c5e95962335f4826d5a0461330055db
Tweakiel <franciaexequiel@hotmail.ca>
2023-03-29 11:25:43 -0400

    added CRUD mutation for memes, setup schema

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,6 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { User } = require('../models');
-const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require("apollo-server-express");
+const { User, Meme, Comment, Favourite } = require("../models");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
@@ -8,8 +8,42 @@ const resolvers = {
       if (context.user) {
         return User.findOne({ _id: context.user._id });
       }
-      throw new AuthenticationError('You need to be logged in!');
-    }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+    // memes query, returns all memes from db and populates the creator, ratings, comments and favorites fields
+    memes: async () => {
+      const memes = await Meme.find()
+        .populate("creator")
+        .populate("ratings")
+        .populate("comments")
+        .populate("favorites");
+      return memes;
+    },
+
+    // meme query, returns a single meme from db and populates the creator, ratings, comments and favorites fields
+    meme: async (parent, { id }) => {
+      const meme = await Meme.findById(id)
+        .populate("creator")
+        .populate("ratings")
+        .populate("comments")
+        .populate("favorites");
+      if (!meme) {
+        throw new Error("Meme not found");
+      }
+      return meme;
+    },
+    // userMemes query, returns all memes created by a user and populates the creator, ratings, comments and favorites fields
+    userMemes: async (parent, args, { user }) => {
+      if (!user) {
+        throw new Error("Authentication required");
+      }
+      const memes = await Meme.find({ creator: user._id })
+        .populate("creator")
+        .populate("ratings")
+        .populate("comments")
+        .populate("favorites");
+      return memes;
+    },
   },
 
   Mutation: {
@@ -22,16 +56,70 @@ const resolvers = {
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
       if (!user) {
-        throw new AuthenticationError('No user found with this email address');
+        throw new AuthenticationError("No user found with this email address");
       }
       const correctPw = await user.isCorrectPassword(password);
       if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
+        throw new AuthenticationError("Incorrect credentials");
       }
       const token = signToken(user);
       return { token, user };
-    }
-  }
+    },
+
+    //add mutation for creating a meme
+    // the creator field is populated with the user._id
+    // creating a new Meme instance with the title and imageUrl arguments
+    // saving the meme to the db
+    createMeme: async (parent, { title, imageUrl }, { user }) => {
+      if (!user) {
+        throw new Error("Authentication required");
+      }
+      const meme = new Meme({ title, imageUrl, creator: user._id });
+      await meme.save();
+      return meme;
+    },
+    //add mutation for updating a meme
+    // finding the meme by id
+    // checking if the meme exists
+    // checking if the user is the creator of the meme
+    // updating the title and imageUrl fields
+    // saving the meme to the db
+    updateMeme: async (parent, { id, title, imageUrl }, { user }) => {
+      if (!user) {
+        throw new Error("Authentication required");
+      }
+      const meme = await Meme.findById(id);
+      if (!meme) {
+        throw new Error("Meme not found");
+      }
+      if (meme.creator.toString() !== user._id.toString()) {
+        throw new Error("Not authorized to update this meme");
+      }
+      meme.title = title || meme.title;
+      meme.imageUrl = imageUrl || meme.imageUrl;
+      await meme.save();
+      return meme;
+    },
+    //add mutation for deleting a meme
+    // finding the meme by id
+    // checking if the meme exists
+    // checking if the user is the creator of the meme
+    // deleting the meme
+    deleteMeme: async (parent, { id }, { user }) => {
+      if (!user) {
+        throw new Error("Authentication required");
+      }
+      const meme = await Meme.findById(id);
+      if (!meme) {
+        throw new Error("Meme not found");
+      }
+      if (meme.creator.toString() !== user._id.toString()) {
+        throw new Error("Not authorized to delete this meme");
+      }
+      await meme.deleteOne();
+      return true;
+    },
+  },
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,25 +1,63 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
   type User {
-    _id: ID 
-    username: String 
-    email: String 
-    password: String 
+    _id: ID
+    username: String
+    email: String
+    password: String
+    memes: [Meme!]!
+    comments: [Comment!]!
+    favorites: [Favorite!]!
   }
 
   type Auth {
     token: ID!
     user: User
   }
+  type Meme {
+    id: ID!
+    title: String!
+    imageUrl: String!
+    creator: User! # 
+    ratings: [Rating!]!
+    comments: [Comment!]!
+    favorites: [Favorite!]!
+  }
+
+  type Comment {
+    id: ID!
+    content: String!
+    author: User!
+    meme: Meme!
+    likes: Int!
+  }
+
+  type Favorite {
+    id: ID!
+    user: User!
+    meme: Meme!
+  }
+
+  type Rating {
+    id: ID!
+    user: User!
+    meme: Meme!
+  }
 
   type Query {
     me: User
+    meme(id: ID!): Meme
+    memes: [Meme!]!
+    userMemes: [Meme!]!
   }
-  
+
   type Mutation {
-    addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): User!
+    createMeme(title: String!, imageUrl: String!): Meme!
+    updateMeme(id: ID!, title: String!, imageUrl: String!): Meme!
+    deleteMeme(id: ID!): Meme!
   }
 `;
 
