01860955bfc4555901ab0f27048ff42562d7558b
Michael <fraynemichael300@gmail.com>
2023-03-29 09:39:17 -0600

    added tailwindcss

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -9,24 +9,29 @@ function Header() {
     Auth.logout();
   };
   return (
-    <div>
-      {Auth.loggedIn() ? (
-            <>
-              <button onClick={logout}>
-                Logout
-              </button>
-            </>
-          ) : (
-            <>
-              <Link to="/login">
-                Login
-              </Link>
-              <Link to="/signup">
-                Signup
-              </Link>
-            </>
-          )}
-    </div>
+    <header className='flex flex-row justify-between'>
+      <h1 className='!w-fit'>
+        Memes Against Humanity
+      </h1>
+      <nav className='w-fit'>
+        {Auth.loggedIn() ? (
+              <>
+                <button onClick={logout}>
+                  Logout
+                </button>
+              </>
+            ) : (
+              <>
+                <Link to="/login">
+                  Login
+                </Link>
+                <Link to="/signup">
+                  Signup
+                </Link>
+              </>
+            )}
+      </nav>
+    </header>
   )
 }
 
