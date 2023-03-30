d9fda43cc3de441bd76cd6b427fcaa0dce354918
Tweakiel <franciaexequiel@hotmail.ca>
2023-03-30 13:54:10 -0400

    added rating mutation, working on add to favourite mutation

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -16,7 +16,8 @@ const typeDefs = gql`
     title: String
     imageUrl: String
     creator: User
-    ratings: Int
+    likes: Int
+    likedBy: [User] # this is an array of users who liked the meme
     comments: [Comment]
     favorites: [Favorite]
   }
@@ -39,30 +40,35 @@ const typeDefs = gql`
     user: User
   }
 
-  type Rating {
-    id: ID!
-    user: User!
-    meme: Meme!
+  type likes { # this is the likes document that is created when a user rates a meme
+    id: ID! # this is the _id of the likes document
+    user: User! # this is the user who created the likes
+    meme: Meme! # this is the meme that was rated
   }
 
   type Query {
-    me: User
-    meme(id: ID!): Meme
-    memes: [Meme!]!
-    userMemes: [Meme!]!
-    getComment(memeId: ID!, commentId: ID!): Comment
-    getCommentsByMemeId(memeId: ID!): [Comment]
+    me: User # returns the current user
+    meme(id: ID!): Meme # returns a single meme by id
+    memes: [Meme!]! # returns all memes
+    userMemes: [Meme!]! # returns all memes created by the current user
+    getComment(memeId: ID!, commentId: ID!): Comment # returns a single comment by memeId and commentId
+    getCommentsByMemeId(memeId: ID!): [Comment] # returns all comments by memeId
   }
 
   type Mutation {
-    login(email: String!, password: String!): Auth
-    addUser(username: String!, email: String!, password: String!): User!
-    createMeme(title: String!, imageUrl: String!): Meme!
-    updateMeme(id: ID!, title: String!, imageUrl: String!): Meme!
-    deleteMeme(id: ID!): Meme!
+    login(email: String!, password: String!): Auth # returns an Auth object containing a token and user
+    addUser(username: String!, email: String!, password: String!): User! # returns a User object
+    createMeme(title: String!, imageUrl: String!): Meme! # returns a Meme object
+    updateMeme(id: ID!, title: String!, imageUrl: String!): Meme! # returns a Meme object
+    deleteMeme(id: ID!): Meme! # returns a Meme object
     addComment(memeId: ID!, commentText: String!): Meme
     updateComment(memeId: ID!, commentId: ID!, commentText: String!): Meme
     removeComment(memeId: ID!, commentId: ID!): Meme
+    addLike(memeId: ID!): Meme
+    removeLike(memeId: ID!): Meme
+    addFavorite(memeId: ID!): Meme
+    removeFavorite(memeId: ID!): Meme
+
   }
 `;
 
