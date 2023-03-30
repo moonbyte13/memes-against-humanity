64ec68b6049e971abae0d6d4e5848bf6f9a29754
Akaydia <akaydiaaa@hotmail.com>
2023-03-30 03:39:55 -0400

    added user query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -2,11 +2,22 @@ import { gql } from '@apollo/client';
 
 export const GET_ME = gql`
   query me {
-  me {
-    _id
-    email
-    password
-    username
+    me {
+      _id
+      email
+      password
+      username
+    }
   }
-}
+`;
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      email
+      password
+      username
+    }
+  }
 `;
